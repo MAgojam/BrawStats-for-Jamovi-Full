802b29be94fa0bda80863f96da892fcc753baa9d
rjwatt42 <83673877+rjwatt42@users.noreply.github.com>
2025-06-27 13:07:21 +0100

    Update brawsim.b.R

--- R/brawsim.b.R
+++ R/brawsim.b.R
@@ -135,7 +135,8 @@ BrawSimClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class(
       showSampleType<-self$options$showSampleType
       showInferParam<-paste0(self$options$inferVar1,";",self$options$inferVar2)
       showInferDimension<-self$options$showInferDimension
-      # self$results$debug$setContent(showInferParam)
+      
+      # self$results$debug$setContent(c(0,self$options$Settings4))
       # self$results$debug$setVisible(TRUE)
       
       # multiple samples
